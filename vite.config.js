3e894c28a81e088aebd7cffa409595781630b9c8
Jason <gt07891@greattree.com.tw>
2022-11-29 16:40:30 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: '/d3-map/',
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            output: {
+                chunkFileNames: 'assets/js/[name]-[hash].js',
+                assetFileNames: 'assets/static/[name]-[hash].[ext]',
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                },
+            },
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: 3000,
